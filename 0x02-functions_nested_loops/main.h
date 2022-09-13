feaaaea0590577cc8cd363631cb8fe0848f85817
utibesky01 <utibesky01@gmail.com>
2022-09-13 13:35:15 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,22 +1,12 @@
-#include "main.h"
-
-/**
- * main - prints the word alx
- *
- * Return: 0 on success execution
- */
-
-int main (void)
-{
-	char s[] = "_putchar";
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar(10);
-
-	return (0);
-}
+int_putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int_islower(int c);
+int_isalpha(int c);
+int print_sign(int n);
+int_abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
