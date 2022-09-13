b86727eebf592e33a5e3f48a49f1f5c123a5fee4
utibesky01 <utibesky01@gmail.com>
2022-09-13 14:46:56 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,7 +13,7 @@ int main(void)
 
 	while (s[i] != '\0')
 	{
-		_putchar(s[i];
+		_putchar(s[i]);
 		i++;
 	}
 	_putchar(10);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,12 @@
-int_putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
-int_abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
+int _abs(int);
+int print_last_digit(int);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
