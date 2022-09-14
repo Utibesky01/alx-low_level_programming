36bebd62c8ce1f127a445922d7614bef00cffe1b
utibesky01 <utibesky01@gmail.com>
2022-09-14 03:07:49 -0700

    9-times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,8 @@ void times_table(void)
 				_putchar(z / 10 + '0');
 				_putchar(z % 10 + '0');
 			}
-			else if (y != 0)			{
+			else if (y != 0)
+			{
 				_putchar(' ');
 				_putchar(z + '0');
 			}
