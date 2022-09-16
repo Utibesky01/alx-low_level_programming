de21a2479f14bd8b03e69be09130e80810bb53d0
utibesky01 <utibesky01@gmail.com>
2022-09-16 10:10:02 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,23 +3,12 @@
 /**
  * _isdigit - function that checks for uppercase character
  * @c: int type number
- *
  *Return: 1 if c is uppercase, 0 otherwise
  */
-
 int _isdigit(int c)
 {
-	char i = '0';
-	int i = 48, isdigit = 0;
-
-	for (; i <= 58; i++)
-	{
-		if (i == c)
-		{
-			isdigit = i;
-			break;
-		}
-	}
-	
-	return (isdigit);
+	if ((c >= 48) && (c <= 57))
+		return (i);
+	else
+		return (0);
 }
