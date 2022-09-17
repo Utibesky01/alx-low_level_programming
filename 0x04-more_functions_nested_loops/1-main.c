f059faeecaf69aee510e551d8341afe081f4e004
utibesky01 <utibesky01@gmail.com>
2022-09-16 18:49:36 -0700

    1-main.c

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - returns result of multiplication between 2 integers
+ * @a: first multiplier
+ * @b: second multiplier
+ *
+ * Return: multiplication result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
