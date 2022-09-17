2e772b2b3f6d0e851b81bcaeba5b12f1a7dc6bc4
utibesky01 <utibesky01@gmail.com>
2022-09-16 18:32:32 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,27 +6,27 @@
  * @n: parameter
  * Return: returns nothing
  */
-
 void print_diagonal(int n)
 {
-	int len, space;
+	int i = 1;
 
 	if (n > 0)
 	{
-		for (len = 0; len < n; len++)
-		{
-			for (space = 0; space < len; space++)
-			{
-				putchar(' ');
-			}
-			putchar('\\');
+		_putchar(92);
 
-			if (len == (n - 1))
+			while (i < n)
 			{
-				continue;
+				int count = 0;
+
+				_putchar('\n');
+			while (count < i)
+			{
+				_putchar(' ');
+				count++;
 			}
-			putchar('\n);
+			_putchar(92);
+			i++;
 		}
 	}
-	putchar('\n');
+	_putchar('\n');
 }
