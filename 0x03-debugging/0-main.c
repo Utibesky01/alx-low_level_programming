27567743368fc209bbe4370ba17be31a613c8eb0
utibesky01 <utibesky01@gmail.com>
2022-09-18 16:08:58 -0700

    0-

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -11,6 +12,7 @@ int main(void)
 
 	i = 98;
 	positive_or_negative(i);
+
 
 	return (0);
 }
