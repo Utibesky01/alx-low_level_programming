b7a232d6f6455bea25fe99416eb5b5379621bc05
utibesky01 <utibesky01@gmail.com>
2022-09-18 17:05:46 -0700

    2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,19 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest = a;
+	int largest;
 
-	if (largest < b)
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
 	{
 		largest = b;
-		if (largest < c)
-			largest = c;
+	}
+	else
+	{
+		largest = c;
 	}
 
 	return (largest);
