2b119bb1bdc8a030bd105164f11a659be2366f73
utibesky01 <utibesky01@gmail.com>
2022-09-20 07:27:18 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,12 +16,11 @@ void rev_string(char *s)
 		length1++;
 
 	length2 = length1 - 1;
-	for (i = 0; i < length1; / 2; i++)
+	for (i = 0; i < length1 / 2; i++)
 	{
 		tmp = s[i];
 		s[i] = s[length2];
 		s[length2] = tmp;
 		length2 -= 1;
-
 	}
 }
