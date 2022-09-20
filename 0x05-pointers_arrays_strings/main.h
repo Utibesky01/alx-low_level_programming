f04075ce8e7ca5be144876a9894b886e475f7651
utibesky01 <utibesky01@gmail.com>
2022-09-20 08:27:14 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN_H'
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -9,6 +9,6 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+char *-strcpy(char *dest, char *src);
 int _atoi(char *s);
 #endif
