d5a4df9acc366100761c8e41d3d3e1d6c76c3e7b
utibesky01 <utibesky01@gmail.com>
2022-09-20 06:20:00 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string -> reversing a string
+ * @s: parameter s
+ */
+void rev_string(char *s)
+{
+	"sam" -> "mas"
+	char tmp;
+	int i, length1, length2;
+
+	length1 = 0;
+	length2 = 0;
+
+	while (s[length1] != '\0')
+		length1++;
+
+	length2 = length1 - 1;
+	for (i = 0; i < length1; i ++)
+	{
+		"samalx" "xlamas"
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 -= 1;
+
+	}
+}
