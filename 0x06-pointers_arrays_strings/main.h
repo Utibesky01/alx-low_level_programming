ba66e95d8f5c2f14228f9c36ebeb6a3946116abb
utibesky01 <utibesky01@gmail.com>
2022-09-23 10:16:40 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,4 +14,6 @@ char *rot13(char *);
 void print_number(int n);
 int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 #endif
