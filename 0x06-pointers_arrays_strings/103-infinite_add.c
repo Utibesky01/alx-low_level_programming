16df52e5e97592c0b4013d464a99343e17aa8295
utibesky01 <utibesky01@gmail.com>
2022-09-23 12:36:27 -0700

    main.h

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,9 +6,9 @@
  * @n2: second number
  * @r: buffer for result
  * @size_r: buffer size
+ *
  * Return: address of r or 0
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
