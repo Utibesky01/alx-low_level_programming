eead3a02e974a99bb5fa7fe3b7d665c6c9ac48ea
utibesky01 <utibesky01@gmail.com>
2022-09-28 08:28:25 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _puts_recursion -> this is a function to print string recursivly
+* _puts_recursion -> this is a function to print string recursivly.
 * @s: string paramenter
 */
 void _puts_recursion(char *s)
