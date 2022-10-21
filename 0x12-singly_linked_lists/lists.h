74f9ca3b13da714c94f7b55cf3b3c0fe277d51f0
utibesky01 <utibesky01@gmail.com>
2022-10-21 14:39:44 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,34 +1,7 @@
-#ifndef _LISTS_
-#define _LISTS_
-
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-} listint_t;
-
-/**
- * struct listp_s - singly linked list
- * @p: pointers of nodes
- * @next: points to the next node
- *
- * Description: singly linked list of pointers
- */
 typedef struct listp_s
 {
-	void *p;
-	struct listp_s *next;
+        void *p;
+        struct listp_s *next;
 } listp_t;
 
 size_t print_listint(const listint_t *h);
@@ -48,3 +21,4 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
+"lists.h" 50L, 1296C   
