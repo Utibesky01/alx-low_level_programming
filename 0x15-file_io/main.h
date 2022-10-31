095729f8573837b46151169361eb6130f832ded2
utibesky01 <utibesky01@gmail.com>
2022-10-31 12:53:42 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,16 @@
-#ifndef _HOLBERTON_
-#define _HOLBERTON_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 int _putchar(char c);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /* HOLBERTON_H */
