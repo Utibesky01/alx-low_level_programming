c94951d62e3a1ab093a2aed7178d438716f79371
utibesky01 <utibesky01@gmail.com>
2023-01-27 13:51:43 -0800

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value from an associated key
+ *
+ * @ht: Hash Table
+ * @key: Key to find its value
+ *
+ * Return: Value associated with the key
+ * Othersise: NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node = NULL;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
+
+	/* Get the index to check */
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node->value);
+		}
+		node = node->next;
+	}
+	return (NULL);
+}
