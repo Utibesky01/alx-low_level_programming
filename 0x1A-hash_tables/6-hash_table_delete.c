692eb0fd58be8b7cca171db4deac33c77777f38c
utibesky01 <utibesky01@gmail.com>
2023-01-27 14:19:02 -0800

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,6 +11,7 @@
 int main(void)
 {
     hash_table_t *ht;
+    char *key;
     char *value;
 
     ht = hash_table_create(1024);
@@ -20,24 +21,19 @@ int main(void)
     hash_table_set(ht, "N", "queens");
     hash_table_set(ht, "Asterix", "Obelix");
     hash_table_set(ht, "Betty", "Cool");
-    hash_table_set(ht, "98", "Battery Street");
-    hash_table_set(ht, "c", "isfun");
-
-    value = hash_table_get(ht, "python");
-    printf("%s:%s\n", "python", value);
-    value = hash_table_get(ht, "Bob");
-    printf("%s:%s\n", "Bob", value);
-    value = hash_table_get(ht, "N");
-    printf("%s:%s\n", "N", value);
-    value = hash_table_get(ht, "Asterix");
-    printf("%s:%s\n", "Asterix", value);
-    value = hash_table_get(ht, "Betty");
-    printf("%s:%s\n", "Betty", value);
-    value = hash_table_get(ht, "98");
-    printf("%s:%s\n", "98", value);
-    value = hash_table_get(ht, "c");
-    printf("%s:%s\n", "c", value);
-    value = hash_table_get(ht, "javascript");
-    printf("%s:%s\n", "javascript", value);
+    hash_table_set(ht, "98", "Battery Streetz");
+    key = strdup("Tim");
+    value = strdup("Britton");
+    hash_table_set(ht, key, value);
+    key[0] = '\0';
+    value[0] = '\0';
+    free(key);
+    free(value);
+    hash_table_set(ht, "98", "Battery Street"); 
+    hash_table_set(ht, "hetairas", "Bob");
+    hash_table_set(ht, "hetairas", "Bob Z");
+    hash_table_set(ht, "mentioner", "Bob");
+    hash_table_set(ht, "hetairas", "Bob Z Chu");
+    hash_table_print(ht);
+    hash_table_delete(ht);
     return (EXIT_SUCCESS);
-}
