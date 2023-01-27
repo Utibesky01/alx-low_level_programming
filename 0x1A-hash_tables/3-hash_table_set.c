9ee367414eb1f971deacb6cbb01ea143f018dcbe
utibesky01 <utibesky01@gmail.com>
2023-01-27 13:45:25 -0800

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,8 @@
 #include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
 
 /**
  * check_key - Checks a particulatr index array to see if the newly added
