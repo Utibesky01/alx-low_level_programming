b49e59a9be769e1508759930f8db880f3945f247
utibesky01 <utibesky01@gmail.com>
2023-01-27 14:15:47 -0800

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -4,38 +4,40 @@
 #include <stdio.h>
 
 /**
- * hash_table_delete - Delete a Hash table
+ * main - check the code
  *
- * @ht: hash table to be deleted
- *
+ * Return: Always EXIT_SUCCESS.
  */
-
-void hash_table_delete(hash_table_t *ht)
+int main(void)
 {
-	unsigned long int i;
-	hash_node_t *node = NULL;
-	hash_node_t *transv = NULL;
+    hash_table_t *ht;
+    char *value;
 
-	if (ht == NULL)
-	{
-		return;
-	}
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "c", "fun");
+    hash_table_set(ht, "python", "awesome");
+    hash_table_set(ht, "Bob", "and Kris love asm");
+    hash_table_set(ht, "N", "queens");
+    hash_table_set(ht, "Asterix", "Obelix");
+    hash_table_set(ht, "Betty", "Cool");
+    hash_table_set(ht, "98", "Battery Street");
+    hash_table_set(ht, "c", "isfun");
 
-	for (i = 0; i < ht->size; i++)
-	{
-		if (ht->array[i] != NULL)
-		{
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				transv = node->next;
-				free(node->value);
-				free(node->key);
-				free(node);
-				node = transv;
-			}
-		}
-	}
-	free(ht->array);
-	free(ht);
+    value = hash_table_get(ht, "python");
+    printf("%s:%s\n", "python", value);
+    value = hash_table_get(ht, "Bob");
+    printf("%s:%s\n", "Bob", value);
+    value = hash_table_get(ht, "N");
+    printf("%s:%s\n", "N", value);
+    value = hash_table_get(ht, "Asterix");
+    printf("%s:%s\n", "Asterix", value);
+    value = hash_table_get(ht, "Betty");
+    printf("%s:%s\n", "Betty", value);
+    value = hash_table_get(ht, "98");
+    printf("%s:%s\n", "98", value);
+    value = hash_table_get(ht, "c");
+    printf("%s:%s\n", "c", value);
+    value = hash_table_get(ht, "javascript");
+    printf("%s:%s\n", "javascript", value);
+    return (EXIT_SUCCESS);
 }
